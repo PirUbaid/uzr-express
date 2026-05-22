f133609f739ea4660b002189505669db1b4a52cf
Pir Ubaid Shah <107417914+pirubaidu@users.noreply.github.com>
2026-05-22 11:21:54 -0700

    app coming soon

--- src/app/pages/download-app/download-app.component.ts
+++ src/app/pages/download-app/download-app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-@Component({standalone:true,template:`
-<section class="page"><h1>download app </h1><p>UZR Express download app page. Use this page for your full business details, forms and customer instructions.</p><a class="btn yellow" href="https://wa.me/923368877657" target="_blank">Contact on WhatsApp</a></section>
+@Component({
+    standalone: true, template: `
+<section class="page"><h1>download app </h1>
+<h2>
+Coming Soon...
+</h2>
+<a class="btn yellow" href="https://wa.me/923368877657" target="_blank">Contact on WhatsApp</a>
+</section>
 `})
-export class DownloadAppComponent {}
+export class DownloadAppComponent { }
